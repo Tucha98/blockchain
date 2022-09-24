46224c618b129f7f2e21fe94d7dbe7eb762510a6
Catarina Julio <Catarinajulioo@gmail.com>
2022-09-24 15:18:50 +0200

    first commit

---
+++ teste.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.0;
+
+contract technology  {
+
+struct cat{
+  string name;
+   uint age;
+}
+
+//Michael[] public Damilola;
+name-of-struct[] public name-of-array;
+cat[] public tucha;
+}
